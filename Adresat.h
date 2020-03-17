2aa28ffe06e26009fae95cee641e628917f8c47a
Rashi <agata.rachanska.programista@gmail.com>
2020-03-17 14:53:18 +0100

    Utworzenie konstruktora dla klasy Adresat

--- Adresat.h
+++ Adresat.h
@@ -19,6 +19,16 @@ class Adresat
 
 public:
     //setery
+    Adresat(int id=0, int idUzytkownika=0, string imie="", string nazwisko="", string numerTelefonu=0, string email="", string adres="")
+    {
+        this->id = id;
+        this->idUzytkownika = idUzytkownika;
+        this->imie = imie;
+        this->nazwisko = nazwisko;
+        this->numerTelefonu = numerTelefonu;
+        this->email = email;
+        this->adres = adres;
+    }
     void ustawID(int noweID);
     void ustawIDUzytkownika(int noweIDUzytkownika);
     void ustawImie(string noweImie);
